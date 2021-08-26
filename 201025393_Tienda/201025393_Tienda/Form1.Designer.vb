72063164b8b027597f7642f7ae4dd16c458fa6aa
Jhimirsonn <89325839+Jhimirsonn@users.noreply.github.com>
2021-08-25 21:40:32 -0600

    Tarea #2

---
+++ 201025393_Tienda/201025393_Tienda/Form1.Designer.vb
@@ -0,0 +1,332 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cantidaddecafe = New System.Windows.Forms.TextBox()
+        Me.cantidaddeazucar = New System.Windows.Forms.TextBox()
+        Me.cantidaddefrijol = New System.Windows.Forms.TextBox()
+        Me.cantidaddearroz = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtpagototal = New System.Windows.Forms.TextBox()
+        Me.txtdescuento = New System.Windows.Forms.TextBox()
+        Me.txtconiva = New System.Windows.Forms.TextBox()
+        Me.txtiva = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtsiniva = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.GroupBox1.BackgroundImage = CType(resources.GetObject("GroupBox1.BackgroundImage"), System.Drawing.Image)
+        Me.GroupBox1.Controls.Add(Me.cantidaddecafe)
+        Me.GroupBox1.Controls.Add(Me.cantidaddeazucar)
+        Me.GroupBox1.Controls.Add(Me.cantidaddefrijol)
+        Me.GroupBox1.Controls.Add(Me.cantidaddearroz)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.GroupBox1.Location = New System.Drawing.Point(41, 46)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(269, 199)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "CANTIDAD COMPRADA"
+        '
+        'cantidaddecafe
+        '
+        Me.cantidaddecafe.Location = New System.Drawing.Point(136, 139)
+        Me.cantidaddecafe.Name = "cantidaddecafe"
+        Me.cantidaddecafe.Size = New System.Drawing.Size(100, 28)
+        Me.cantidaddecafe.TabIndex = 7
+        '
+        'cantidaddeazucar
+        '
+        Me.cantidaddeazucar.Location = New System.Drawing.Point(136, 102)
+        Me.cantidaddeazucar.Name = "cantidaddeazucar"
+        Me.cantidaddeazucar.Size = New System.Drawing.Size(100, 28)
+        Me.cantidaddeazucar.TabIndex = 6
+        '
+        'cantidaddefrijol
+        '
+        Me.cantidaddefrijol.Location = New System.Drawing.Point(136, 65)
+        Me.cantidaddefrijol.Name = "cantidaddefrijol"
+        Me.cantidaddefrijol.Size = New System.Drawing.Size(100, 28)
+        Me.cantidaddefrijol.TabIndex = 5
+        '
+        'cantidaddearroz
+        '
+        Me.cantidaddearroz.Location = New System.Drawing.Point(136, 28)
+        Me.cantidaddearroz.Name = "cantidaddearroz"
+        Me.cantidaddearroz.Size = New System.Drawing.Size(100, 28)
+        Me.cantidaddearroz.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft YaHei UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(19, 142)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Lb. de Café"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(17, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Lb. de Azúcar"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(17, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Lb. de Frijol"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(17, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Lb. de Arroz"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.GroupBox2.BackgroundImage = CType(resources.GetObject("GroupBox2.BackgroundImage"), System.Drawing.Image)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.txtpagototal)
+        Me.GroupBox2.Controls.Add(Me.txtdescuento)
+        Me.GroupBox2.Controls.Add(Me.txtconiva)
+        Me.GroupBox2.Controls.Add(Me.txtiva)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.txtsiniva)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.GroupBox2.Location = New System.Drawing.Point(337, 36)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(292, 235)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "RESULTADOS"
+        Me.GroupBox2.Visible = False
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft YaHei UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(25, 188)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(105, 20)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "PAGO TOTAL"
+        '
+        'txtpagototal
+        '
+        Me.txtpagototal.Location = New System.Drawing.Point(161, 190)
+        Me.txtpagototal.Name = "txtpagototal"
+        Me.txtpagototal.Size = New System.Drawing.Size(100, 28)
+        Me.txtpagototal.TabIndex = 12
+        '
+        'txtdescuento
+        '
+        Me.txtdescuento.Location = New System.Drawing.Point(161, 147)
+        Me.txtdescuento.Name = "txtdescuento"
+        Me.txtdescuento.Size = New System.Drawing.Size(100, 28)
+        Me.txtdescuento.TabIndex = 11
+        '
+        'txtconiva
+        '
+        Me.txtconiva.Location = New System.Drawing.Point(161, 107)
+        Me.txtconiva.Name = "txtconiva"
+        Me.txtconiva.Size = New System.Drawing.Size(100, 28)
+        Me.txtconiva.TabIndex = 10
+        '
+        'txtiva
+        '
+        Me.txtiva.Location = New System.Drawing.Point(161, 71)
+        Me.txtiva.Name = "txtiva"
+        Me.txtiva.Size = New System.Drawing.Size(100, 28)
+        Me.txtiva.TabIndex = 9
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft YaHei UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(25, 150)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(99, 20)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "DESCUENTO"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft YaHei UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(63, 75)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(34, 20)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "IVA"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft YaHei UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(25, 110)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(119, 20)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "PAGO CON IVA"
+        '
+        'txtsiniva
+        '
+        Me.txtsiniva.Location = New System.Drawing.Point(161, 32)
+        Me.txtsiniva.Name = "txtsiniva"
+        Me.txtsiniva.Size = New System.Drawing.Size(100, 28)
+        Me.txtsiniva.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft YaHei UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(25, 35)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(110, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "PAGO SIN IVA"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Maroon
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button1.Location = New System.Drawing.Point(114, 318)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 35)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "CALCULAR"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Maroon
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button2.Location = New System.Drawing.Point(288, 318)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(121, 35)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "LIMPIAR"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Maroon
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button3.Location = New System.Drawing.Point(463, 318)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(121, 35)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "SALIR"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(680, 422)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents cantidaddecafe As TextBox
+    Friend WithEvents cantidaddeazucar As TextBox
+    Friend WithEvents cantidaddefrijol As TextBox
+    Friend WithEvents cantidaddearroz As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtpagototal As TextBox
+    Friend WithEvents txtdescuento As TextBox
+    Friend WithEvents txtconiva As TextBox
+    Friend WithEvents txtiva As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtsiniva As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ 201025393_Tienda/201025393_Tienda/Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Math
+Public Class Form1
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        End
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        GroupBox2.Visible = False
+
+        cantidaddearroz.Clear()
+        cantidaddefrijol.Clear()
+        cantidaddeazucar.Clear()
+        cantidaddecafe.Clear()
+        txtsiniva.Clear()
+        txtiva.Clear()
+        txtconiva.Clear()
+        txtdescuento.Clear()
+        txtpagototal.Clear()
+
+        cantidaddearroz.Focus()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim arroz As Double
+        Dim frijol As Double
+        Dim azucar As Double
+        Dim cafe As Double
+        Dim pagosiniva As Double
+        Dim iva As Double
+        Dim pagoconiva As Double
+        Dim descuento As Double
+        Dim totalpago As Double
+
+        Const a = 2
+        Const b = 1.75
+        Const c = 2.5
+        Const d = 0.12
+        Const f = 1.25
+        Const g = 0.025
+
+        arroz = Val(cantidaddearroz.Text)
+        frijol = Val(cantidaddefrijol.Text)
+        azucar = Val(cantidaddeazucar.Text)
+        cafe = Val(cantidaddecafe.Text)
+
+        pagosiniva = (arroz * a) + (frijol * b) + (azucar * c) + (cafe * f)
+        iva = pagosiniva * d
+        pagoconiva = (pagosiniva * d) + pagosiniva
+        descuento = pagoconiva * g
+        totalpago = pagoconiva - descuento
+
+        GroupBox2.Visible = True
+
+        txtsiniva.Text = Str(Round(pagosiniva, 2))
+        txtiva.Text = Str(Round(iva, 2))
+        txtconiva.Text = Str(Round(pagoconiva, 2))
+        txtdescuento.Text = Str(Round(descuento, 2))
+        txtpagototal.Text = Str(Round(totalpago, 2))
+
+    End Sub
+End Class
